58f58bf460c469055b50842397381e5abbc05aa3
XardnoX <ondra.kraja@gmail.com>
2023-01-06 11:54:40 +0100

    update 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = JOptionPane.showInputDialog("zde napiš své jméno");
+        JOptionPane.showMessageDialog(null, "Ahoj " + name);
+        int vek = Integer.parseInt(JOptionPane.showInputDialog("zde napiš svůj věk"));
+        JOptionPane.showMessageDialog(null, "máš " + vek + " let");
+        double vyska = Double.parseDouble(JOptionPane.showInputDialog("Jak jsi vysoký?"));
+        JOptionPane.showMessageDialog(null, "Jsi " + vyska + "cm vysoký");
     }
 }
